e549492a0ad970236c6189236579d220717f45d4
Gshadow6 <axelshadow7@gmail.com>
2021-03-19 17:46:04 -0600

    19/03/21 Tarea 4: Agregar objetos miMustang y miAcura

--- ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -25,6 +25,21 @@ public class ConceptosBasicos {
         miBocho.setModelo(1970);
         miBocho.setColor(Color.blue);
         System.out.println(miBocho);
+        
+        //Tarea: Programar los objetos myAcura y myMustang}
+        Automovil miMustang= new Automovil();
+        miMustang.setMarca("Ford");
+        miMustang.setSubmarca("Mustang");
+        miMustang.setModelo(2010);
+        miMustang.setColor(Color.yellow);
+        System.out.println(miMustang);
+        
+        Automovil miAcura= new Automovil();
+        miAcura.setMarca("Acura");
+        miAcura.setSubmarca("NSX");
+        miAcura.setModelo(2013);
+        miAcura.setColor(Color.gray);
+        System.out.println(miAcura);
     }
     
 }
